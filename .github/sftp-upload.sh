aed88921421fd4e0f863dd7dc55aa3346db21262
PreFiXAUT <mail@prefix.moe>
2019-09-29 22:58:58 +0200

    Escaped script parameters more

--- .github/sftp-upload.sh
+++ .github/sftp-upload.sh
@@ -2,8 +2,8 @@
 
 $KEY_FILE=/tmp/sftp_rsa
 echo "Creating SFTP Key ..."
-echo cat $SFTP_KEY | base64 --decode > $KEY_FILE
+echo "${SFTP_KEY}" | base64 --decode > $KEY_FILE
+chmod 600 "${KEY_FILE}"
 echo "Created SFTP Key-File at: $KEY_FILE"
-chmod 600 $KEY_FILE
 echo "Uploading docs to Server ..."
-scp -i "${KEY_FILE}" -o stricthostkeychecking=no ./dist/**/* ${SFTP_USER}@${SFTP_HOST}:${SFTP_PATH}
+scp -i "${KEY_FILE}" -o "stricthostkeychecking=no" ./dist/**/* ${SFTP_USER}@${SFTP_HOST}:${SFTP_PATH}
