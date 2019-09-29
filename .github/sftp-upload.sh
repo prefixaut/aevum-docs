1ad249fdd3d8b9bfa0b4b54dff991dc3f583525c
PreFiXAUT <mail@prefix.moe>
2019-09-29 21:52:53 +0200

    Escaped key-file variable

--- .github/sftp-upload.sh
+++ .github/sftp-upload.sh
@@ -6,4 +6,4 @@ echo cat $SFTP_KEY | base64 --decode > $KEY_FILE
 echo "Created SFTP Key-File at: $KEY_FILE"
 chmod 600 $KEY_FILE
 echo "Uploading docs to Server ..."
-scp -i $KEY_FILE -o stricthostkeychecking=no ./dist/**/* ${SFTP_USER}@${SFTP_HOST}:${SFTP_PATH}
+scp -i "${KEY_FILE}" -o stricthostkeychecking=no ./dist/**/* ${SFTP_USER}@${SFTP_HOST}:${SFTP_PATH}
