a4385552567573bbb59771137d6ede4bef7c615f
PreFiXAUT <mail@prefix.moe>
2019-09-29 21:04:43 +0200

    Added file-permission change to upload-script

--- .github/sftp-upload.sh
+++ .github/sftp-upload.sh
@@ -2,5 +2,6 @@
 
 echo "Creating SFTP Key ..."
 echo "${SFTP_KEY}" | base64 --decode > /tmp/sftp_rsa
+chmod 600 /tmp/sftp_rsa
 echo "Uploading docs to Server ..."
 scp -i /tmp/sftp_rsa -o stricthostkeychecking=no ./dist/**/* ${SFTP_USER}@${SFTP_HOST}:${SFTP_PATH}
