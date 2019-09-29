1011210d5e65dd443eff2175b8a18a3f97c5822c
PreFiXAUT <mail@prefix.moe>
2019-09-29 20:52:44 +0200

    Updated deploy script to use key-file

--- .github/sftp-upload.sh
+++ .github/sftp-upload.sh
@@ -3,4 +3,4 @@
 echo "Creating SFTP Key ..."
 echo "${SFTP_KEY}" | base64 --decode /tmp/sftp_rsa
 echo "Uploading docs to Server ..."
-scp -i ./dist/**/* ${SFTP_USER}@${SFTP_HOST}:${SFTP_PATH}
+scp -i /tmp/sftp_rsa ./dist/**/* ${SFTP_USER}@${SFTP_HOST}:${SFTP_PATH}
