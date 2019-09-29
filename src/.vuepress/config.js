1148f88a5aea4731d79b43613393a2cf3a574d19
PreFiXAUT <mail@prefix.moe>
2019-09-29 18:14:45 +0200

    Moved docs from aevum repository into own one

---
+++ src/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    base: '/aevum/',
+    title: 'Aevum',
+    description: 'Documentation for the aevum javascript-library',
+    themeConfig: {
+        repo: 'prefixaut/aevum',
+        repoLabel: 'GitHub',
+        docsDir: 'docs',
+        docsBranch: 'master',
+        editLinks: true,
+        editLinkText: 'Improve this Page!',
+        lastUpdated: 'Last Updated',
+        sidebar: [
+            '/',
+            '/installation.md',
+            '/building.md',
+            '/usage.md',
+        ]
+    }
+};
