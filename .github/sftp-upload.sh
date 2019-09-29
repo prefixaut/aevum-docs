cf38c543870b6ff06806929724e5a895561e3445
PreFiXAUT <mail@prefix.moe>
2019-09-29 21:20:59 +0200

    Added sftp-key location as variable

--- .github/sftp-upload.sh
+++ .github/sftp-upload.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+$KEY_FILE=/tmp/sftp_rsa
 echo "Creating SFTP Key ..."
-echo "${SFTP_KEY}" | base64 --decode > /tmp/sftp_rsa
-chmod 600 /tmp/sftp_rsa
+echo cat $SFTP_KEY | base64 --decode > $KEY_FILE
+echo "Created SFTP Key-File at: $KEY_FILE"
+chmod 600 $KEY_FILE
 echo "Uploading docs to Server ..."
-scp -i /tmp/sftp_rsa -o stricthostkeychecking=no ./dist/**/* ${SFTP_USER}@${SFTP_HOST}:${SFTP_PATH}
+scp -i $KEY_FILE -o stricthostkeychecking=no ./dist/**/* ${SFTP_USER}@${SFTP_HOST}:${SFTP_PATH}
